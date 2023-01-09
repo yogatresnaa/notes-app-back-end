dcdda264e5f5a0701ebec421ee67b92a687b12ec
yoga tresna <yogatresnaa@gmail.com>
2023-01-09 10:37:55 +0700

    create table database with teknik migrate, config RDBMS Postgres

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
+/* eslint-disable max-len */
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesService');
+const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
+// mengimpor dotenv dan menjalankan konfigurasinya
+require('dotenv').config();
 
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
@@ -22,6 +25,12 @@ const init = async () => {
       validator: NotesValidator,
     },
   });
+
+  // Di sini kamu bisa mendeklarasikan atau membuat extentions function untuk life cycle server onPreResponse, di mana ia akan mengintervensi response sebelum dikirimkan ke client. Di sana kamu bisa menetapkan error handling bila response tersebut merupakan client error.
+
+  //  server.ext('onPreResponse', (request, h) => { // mendapatkan konteks response dari request const { response } = request; if (response instanceof ClientError) { // membuat response baru dari response toolkit sesuai kebutuhan error handling const newResponse = h.response({ status: 'fail', message: response.message, }); newResponse.code(response.statusCode); return newResponse; } // jika bukan ClientError, lanjutkan dengan response sebelumnya (tanpa terintervensi) return response.continue || response; });
+
+  //   Dengan begitu, di handler, kamu bisa fokus terhadap logika dalam menangani request, tanpa adanya error handling.
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
